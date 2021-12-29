75980da2d2ea77e54e6f068b4fe42d06f371fe34
Tao Long <longtao.cmu@gmail.com>
2021-12-28 23:34:46 -0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import RippleElement from "./RippleElement";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <RippleElement>
-      <div
-        style={{ backgroundColor: "black", height: "30px", width: "90px" }}
-      ></div>
-    </RippleElement>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <RippleElement>
+//       <div
+//         style={{ backgroundColor: "black", height: "30px", width: "90px" }}
+//       ></div>
+//     </RippleElement>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 export default RippleElement;
